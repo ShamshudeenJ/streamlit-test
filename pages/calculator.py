687ae07dab9a815786d251f8d224f0f502805a5e
Shamshudeen J <j.shamshudeen@gmail.com>
2023-09-02 15:46:16 +0530

    Update calculator.py

--- pages/calculator.py
+++ pages/calculator.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-number1 = st.number_input('Insert a number')
-number2 = st.number_input('Insert a number')
+number1 = st.number_input('Insert a number 1: ')
+number2 = st.number_input('Insert a number 2: ')
 st.write('Addition: ', str(number1 + number2))
